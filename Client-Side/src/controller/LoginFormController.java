940d10e95eb13a40d4354f71ae9091d7387c0cf5
udyogi-siphara <udyogisiphara22@gmail.com>
2022-08-06 14:26:39 -0700

    Create Login From

--- Client-Side/src/controller/LoginFormController.java
+++ Client-Side/src/controller/LoginFormController.java
@@ -7,6 +7,31 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
 
+    public JFXTextField txtUserName;
+    public Label lblSignup;
+    public FontAwesomeIconView icnEye;
+    public JFXPasswordField pwdPassword;
+    public JFXTextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void passwordFieldValidationOnAction(KeyEvent keyEvent) {
+    }
+
+    public void eyeClickOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void textFieldValidationOnAction(KeyEvent keyEvent) {
+    }
 }
