82c3d9072940c040c44f8fa8fe4bbc9114def40d
udyogi-siphara <udyogisiphara22@gmail.com>
2022-08-06 16:01:35 -0700

    Create ChatRoom Form

--- Client-Side/src/controller/ChatRoomFormController.java
+++ Client-Side/src/controller/ChatRoomFormController.java
@@ -7,6 +7,26 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXTextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
 public class ChatRoomFormController {
 
+    public Circle proPic;
+    public Label userName;
+    public Pane profile;
+    public Pane chat;
+    public JFXTextArea txtChatbox;
+    public TextField txtmsg;
+
+    public void sendMessageByKey(KeyEvent keyEvent) {
+    }
+
+    public void handleSendEvent(MouseEvent mouseEvent) {
+    }
 }
