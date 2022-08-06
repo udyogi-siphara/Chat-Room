822c25e94f997ec5bc11c07ef2c7374d192ffc71
udyogi-siphara <udyogisiphara22@gmail.com>
2022-08-06 15:00:08 -0700

    Create Signup form

--- Client-Side/src/controller/SignupFormController.java
+++ Client-Side/src/controller/SignupFormController.java
@@ -7,6 +7,36 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class SignupFormController {
 
+    public JFXTextField txtUserName;
+    public Label lblLogin;
+    public FontAwesomeIconView icnEye;
+    public JFXPasswordField pwdPassword;
+    public JFXTextField txtPassword;
+    public JFXTextField txtFullName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPhoneNumber;
+    public ImageView imgBack;
+
+    public void btnSignupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFieldValidationOnAction(KeyEvent keyEvent) {
+    }
+
+    public void eyeClickOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void passwordFieldValidationOnAction(KeyEvent keyEvent) {
+    }
 }
