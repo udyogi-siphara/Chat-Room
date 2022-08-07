7b6bea8ad3ca4382d25e0be7cc30cd11c771030e
udyogi-siphara <udyogisiphara22@gmail.com>
2022-08-06 22:29:06 -0700

    Code in serverAppinitializer and Client Handler

--- Server-Side/src/AppInitializerServer.java
+++ Server-Side/src/AppInitializerServer.java
@@ -1,14 +1,33 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class AppInitializerServer extends Application {
+public class AppInitializerServer{
+
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private static final int PORT=5000;
+    private static ServerSocket serverSocket;
+    private static Socket accept;
 
     public static void main(String[] args) {
-        launch(args);
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                System.out.println("Waiting for clients...");
+                accept= serverSocket.accept();
+                System.out.println("Client Connected...");
+                ClientHandler clientThread = new ClientHandler(accept, clientHandlers);
+                clientHandlers.add(clientThread);
+                clientThread.start();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 
-    @Override
-    public void start(Stage primaryStage) {
 
-    }
 }
--- Server-Side/src/ClientHandler.java
+++ Server-Side/src/ClientHandler.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * @author : Udyogi Siphara
  * Project Name: Chat-Room
@@ -5,6 +12,20 @@
  * Time        : 1:22 PM
  */
 
-public class ClientHandler {
+public class ClientHandler extends Thread {
+    private ArrayList<ClientHandler> clientHandlers;
+    private Socket accept;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
+    public ClientHandler(Socket accept, ArrayList<ClientHandler> clientHandlers) {
+        try {
+            this.accept = accept;
+            this.clientHandlers = clientHandlers;
+            this.reader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+            this.writer = new PrintWriter(accept.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
