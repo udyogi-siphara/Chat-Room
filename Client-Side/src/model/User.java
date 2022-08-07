e442d3dbd5dfed7ec2de2c9bbef7bea4c40c95ff
udyogi-siphara <udyogisiphara22@gmail.com>
2022-08-06 23:10:27 -0700

    Added Validation util class and User model

---
+++ Client-Side/src/model/User.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Chat-Room
+ * Date        : 8/6/2022
+ * Time        : 11:07 PM
+ */
+
+package model;
+
+public class User {
+
+}
---
+++ Client-Side/src/util/ValidationUtil.java
@@ -0,0 +1,52 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Chat-Room
+ * Date        : 8/6/2022
+ * Time        : 11:07 PM
+ */
+
+package util;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
+
+public class ValidationUtil {
+    public static Object validateJFXTextField(LinkedHashMap<JFXTextField, Pattern> map, JFXButton btn) {
+        btn.setDisable(true);
+        for (JFXTextField textFieldKey : map.keySet()) {
+            System.out.println("textFieldKey");
+            System.out.println(textFieldKey);
+            Pattern patternValue = map.get(textFieldKey);
+            if (!patternValue.matcher(textFieldKey.getText()).matches()) {
+                if (!textFieldKey.getText().isEmpty()) {
+                    textFieldKey.setStyle("-fx-text-fill: red");
+                }
+                return textFieldKey;
+            }
+            textFieldKey.setStyle("-fx-text-fill: blue");
+        }
+        btn.setDisable(false);
+        return true;
+    }
+
+    public static Object validateJFXPasswordField(LinkedHashMap<JFXPasswordField, Pattern> map, JFXButton btn) {
+        btn.setDisable(true);
+        for (JFXPasswordField passwordFieldKey : map.keySet()) {
+            Pattern patternValue = map.get(passwordFieldKey);
+            if (!patternValue.matcher(passwordFieldKey.getText()).matches()) {
+                if (!passwordFieldKey.getText().isEmpty()) {
+                    passwordFieldKey.setStyle("-fx-text-fill: red");
+                }
+                return passwordFieldKey;
+            }
+            passwordFieldKey.setStyle("-fx-text-fill: white");
+        }
+        btn.setDisable(false);
+        return true;
+    }
+
+}
